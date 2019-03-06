14b51cf413907cb458b3cdc2f7ea0f128abf7734
Artie Negron <anegroncsu@gmail.com>
2019-03-06 10:40:14 -0500

    Added tests for inputs '0', '1', and '2'. Refactored.

--- StringCalculatorKata.Tests/StringCalculatorTests.cs
+++ StringCalculatorKata.Tests/StringCalculatorTests.cs
@@ -1,18 +1,33 @@
-using System;
 using Xunit;
 
 namespace StringCalculatorKata.Tests
 {
     public class StringCalculatorTests
     {
+        private StringCalculator underTest;
+
+        public StringCalculatorTests()
+        {
+            underTest = new StringCalculator();
+        }
+
         [Fact]
         public void Add_Returns_0_For_Empty_String()
         {
-            var underTest = new StringCalculator();
+            var expected = 0;
+            var result = underTest.Add("");
 
-            var expected = underTest.Add("");
+            Assert.Equal(expected, result);
+        }
 
-            Assert.Equal(0, expected);
+        [Theory]
+        [InlineData("1", 1)]
+        [InlineData("2", 2)]
+        public void Add_Returns_Expected_Output(string input, int expected)
+        {
+            var result = underTest.Add(input);
+
+            Assert.Equal(expected, result);
         }
     }
 }
